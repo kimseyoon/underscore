9295d3596a4c8baf3d8f94b085feb105b6716770
ksy-PC\ksy <kimse7533@naver.com>
2017-06-16 00:00:28 +0900

    Update each, map, reduce function 구현
    each, map, reduce 함수 구현 및 Unit Test

--- underscore.js
+++ underscore.js
@@ -7,6 +7,43 @@ parameter : (list[Array or Object], iteratee)
 return : array
 */
   return {
+    each : function(list, cb){
+      var i;
+      if(Array.isArray(list)){
+        for(i = 0; i < list.length; i++){
+          cb(list[i]);
+        }
+      }else{
+        for(var key in list){
+          cb(list[key]);
+        }
+      }
+    },
+
+    map : function(list, cb){
+      var i,
+          result = [];
+      if(Array.isArray(list)){
+        for(i = 0; i < list.length; i++){
+          result.push(cb(list[i]));
+        }
+      }else{
+        for(var key in list){
+          result.push(cb(list[key]));
+        }
+      }
+      return result;
+    },
+
+    reduce : function(list, cb, memo){
+      var i,
+          result = 0;
+      for(i = 0; i < list.length; i++){
+        memo = cb(memo, list[i]);
+      }
+      result = memo;
+      return result;
+    }
   }
 
 })(window)
---
+++ underscore_test.js
@@ -0,0 +1,43 @@
+var assert = chai.assert;
+
+
+// _.each([1, 2, 3], alert);
+//_.each({"one":2, "two":3}, alert);
+
+// describe("_.each function Test",function(){
+//     it("0ms Type Check ", function(){
+//         let result = _.each(duration)
+//         assert.equal(result,  "00:00:00");
+//
+//         // _.each([1, 2, 3], alert);
+//         //_.each({"one":2, "two":3}, alert);
+//     });
+// });
+
+describe("_.map function Test",function(){
+    it("if parameter type is array  ", function(){
+        var result = _.map([1,2,3], function(num){
+          return num*2
+        })
+        assert.deepEqual(result, [2,4,6]);
+    });
+
+    it("if parameter type is object", function(){
+        var result = _.map({"one":2, "two":3}, function(num){
+          return num*2
+        })
+        assert.deepEqual(result, [4,6]);
+    });
+});
+
+describe("_.reduce function Test",function(){
+    it("two-dimensional array concat", function(){
+        var result = _.reduce([[0, 4], [2, 1], [4, 5]], function(a, b) { return a.concat(b); }, [])
+        assert.deepEqual(result, [0,4,2,1,4,5]);
+    });
+
+    it("every number sum", function(){
+        var result = _.reduce([1, 2, 3], function(memo, num){ return memo + num; }, 0)
+        assert.deepEqual(result, 6);
+    });
+});
