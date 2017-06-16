fe666449e82a1ce9529c8ce0ee9bfde0ce633557
ksy-PC\ksy <kimse7533@naver.com>
2017-06-16 15:58:27 +0900

    Update 모듈노출패턴 으로 변경
    공개로 하고싶은 API들만 return 값으로 노출 시키는 방법으로 바꿈

--- underscore.js
+++ underscore.js
@@ -1,112 +1,123 @@
 "use strict"
 
-var _ = (function(window){
+var _ = window._ || {};
+
+_ = (function(window){
   var global = window;
 /*
 parameter : (list[Array or Object], iteratee)
 return : array
 */
-  return {
-    each : function(list, cb){
-      var i,
-          listLength = list.length;
-      if(Array.isArray(list)){
-        for(i = 0; i < listLength; i++){
-          cb(list[i]);
-        }
-      }else{
-        for(var key in list){
-          cb(list[key]);
-        }
-      }
-    },
 
-    map : function(list, cb){
-      var i,
-          listLength = list.length,
-          result = [];
-      if(Array.isArray(list)){
-        for(i = 0; i < listLength; i++){
-          result.push(cb(list[i]));
-        }
-      }else{
-        for(var key in list){
-          result.push(cb(list[key]));
-        }
-      }
-      return result;
-    },
-
-    reduce : function(list, cb, memo){
-      var i,
-          listLength = list.length,
-          result = 0;
+  var each = function(list, cb){
+    var i,
+        listLength = list.length;
+    if(Array.isArray(list)){
       for(i = 0; i < listLength; i++){
-        memo = cb(memo, list[i]);
+        cb(list[i]);
       }
-      result = memo;
-      return result;
-    },
-
-    reduceRight : function(list, cb, memo){
-      var i,
-          listLength = list.length,
-          result = 0;
-      for(i = listLength - 1; i >= 0; i--){
-        memo = cb(memo, list[i]);
+    }else{
+      for(var key in list){
+        cb(list[key]);
       }
-      result = memo;
-      return result;
-    },
+    }
+  }
 
-    find : function(list, cb){
-      var i,
-          listLength = list.length;
-
-      for(i = 0 ; i < listLength; i++){
-        if(cb(list[i])){
-          return list[i]
-        }
-      }
-    },
-
-    filter : function(list, cb){
-      var i,
-          listLength = list.length,
-          result = [];
-
+  var map = function(list, cb){
+    var i,
+        listLength = list.length,
+        result = [];
+    if(Array.isArray(list)){
       for(i = 0; i < listLength; i++){
-        if(cb(list[i])){
-          result.push(list[i])
-        }
+        result.push(cb(list[i]));
       }
+    }else{
+      for(var key in list){
+        result.push(cb(list[key]));
+      }
+    }
+    return result;
+  }
 
-      return result;
-    },
+  var reduce = function(list, cb, memo){
+    var i,
+        listLength = list.length,
+        result = 0;
+    for(i = 0; i < listLength; i++){
+      memo = cb(memo, list[i]);
+    }
+    result = memo;
+    return result;
+  }
 
-    where : function(list, properties){
-      var i,
-          listLength = list.length,
-          result = [];
+  var reduceRight = function(list, cb, memo){
+    var i,
+        listLength = list.length,
+        result = 0;
+    for(i = listLength - 1; i >= 0; i--){
+      memo = cb(memo, list[i]);
+    }
+    result = memo;
+    return result;
+  }
 
-      for(i = 0 ; i < listLength; i++){
-        var isHas = false;
-        for(var key in properties){
-          if(list[i].hasOwnProperty(key)){
-            if(properties[key] === list[i][key]){
-              isHas = true;
-            }else{
-              isHas = false;
-              break;
-            }
+  var find = function(list, cb){
+    var i,
+        listLength = list.length;
+
+    for(i = 0 ; i < listLength; i++){
+      if(cb(list[i])){
+        return list[i]
+      }
+    }
+  }
+
+  var filter = function(list, cb){
+    var i,
+        listLength = list.length,
+        result = [];
+
+    for(i = 0; i < listLength; i++){
+      if(cb(list[i])){
+        result.push(list[i])
+      }
+    }
+
+    return result;
+  }
+
+  var where = function(list, properties){
+    var i,
+        listLength = list.length,
+        result = [];
+
+    for(i = 0 ; i < listLength; i++){
+      var isHas = false;
+      for(var key in properties){
+        if(list[i].hasOwnProperty(key)){
+          if(properties[key] === list[i][key]){
+            isHas = true;
+          }else{
+            isHas = false;
+            break;
           }
         }
-        if(isHas === true){
-          result.push(list[i]);
-        }
       }
-      return result;
+      if(isHas === true){
+        result.push(list[i]);
+      }
     }
+    return result;
+  }
+
+  return {
+    "each" : each,
+    "map" : map,
+    "reduce" : reduce,
+    "reduceRight" : reduceRight,
+    "find" : find,
+    "filter" : filter,
+    "where" : where
   }
 
 })(window)
